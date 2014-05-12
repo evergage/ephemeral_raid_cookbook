45b6a699180e8e1ead8122d238677313ca314441
Alex Trull <alextrull@getchef.com>
2014-05-12 16:59:25 +0100

    Bumped cookbook and fixed maintainer version.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'ephemeral_raid'
-maintainer       'Medidata Solutions, Inc.'
-maintainer_email 'cookbooks@mdsol.com'
+maintainer       'Alex Trull'
+maintainer_email 'cookbooks.alex@trull.org'
 license          'Apache 2.0'
 description      'Creates Dynamic Ephemeral Raids on EC2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.6'
+version          '0.0.7'
 
 %w{ ubuntu debian redhat fedora centos scientific amazon }.each do |os|
   supports os
